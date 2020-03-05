8ad9f22d2485cff44a71d25635583a7e896b42ab
Yuliya Karenik <mionu@users.noreply.github.com>
2020-03-05 13:54:20 +0300

    Homework 6 (#5)
    * plain text & html servers
    
    * json & echo servers, middlewares
    
    * routes
    
    * refactor
    
    * jwt tokens
    
    * local & facebook passport strategies
    
    * twitter & google strategies
    
    * fix paths to routes
    
    * homework 6

---
+++ migrations/20171114160017-create-product.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      brand: {
+        type: Sequelize.STRING
+      },
+      company: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.STRING
+      },
+      isbn: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('products');
+  }
+};
